21e56e01ce089512754a8b96e2e4f4dcd124aea6
k-rsouza <k.souza@edu.pucrs.br>
2025-09-16 20:35:35 -0300

    Adding SocketIO and payment page

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ from repository.database import db
 from db_models.payment import Payment
 from datetime import datetime, timedelta
 from payments.pix import Pix    
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'key'
 
 db.init_app(app)
+socketio = SocketIO(app)
 
 @app.route('/payments/pix', methods=['POST'])
 def create_payment_pix():
@@ -37,18 +39,46 @@ def get_image(file_name):
 
 @app.route('/payments/pix/confirmation', methods=['POST'])
 def pix_confirmation():
-    return jsonify({
-        "message": "The payment has been confirmed"
-    })
+    data = request.get_json()
+
+    #validations
+    if "bank_payment_id" not in data and "value" not in data:
+        return jsonify({"message": "Invalid payment data"}), 400
+
+    # recuperando payment
+    payment = Payment.query.filter_by(bank_payment_id=data.get("bank_payment_id")).first()
+
+    if not payment or payment.paid:
+       return jsonify({"message": "Payment no found"}), 404
+
+    if data.get("value") != payment.value:
+        return jsonify({"message": "Invalid payment data"}), 400
+    
+    payment.paid = True
+    db.session.commit()
+    socketio.emit(f"payment-confirmed-{payment.id}")
+
+    return jsonify({"message": "The payment has been confirmed"})
 
 @app.route('/payments/pix/<int:payment_id>', methods=['GET'])
 def payment_pix_page(payment_id):
     payment = Payment.query.get(payment_id)
 
+    if payment.paid:
+        return render_template('confirmed_payment.html', payment_id=payment_id,
+                               value=payment.value)
+    
     return render_template('payment.html', payment_id=payment.id, 
                            value=payment.value, 
                            host="http://127.0.0.1:5000", 
                            qr_code=payment.qr_code)
 
+
+# websockets
+@socketio.on('connect')
+def handle_connect():
+    print("Client connected to the server")
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
