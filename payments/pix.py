ef83452c7a843cbeb04de006a2656aed1a170b1c
k-rsouza <k.souza@edu.pucrs.br>
2025-09-17 22:51:37 -0300

    Implementing tests, and 404 Error page

--- payments/pix.py
+++ payments/pix.py
@@ -5,7 +5,7 @@ class Pix:
     def __init__(self):
         pass
 
-    def create_payment(self):
+    def create_payment(self, base_dir=""):
         bank_payment_id = str(uuid.uuid4())
 
         # "copia e cola"
@@ -13,7 +13,7 @@ class Pix:
 
         #qr code
         img = qrcode.make(hash_payment)
-        img.save(f"static/img/qr_code_payment_{bank_payment_id}.png")
+        img.save(f"{base_dir}static/img/qr_code_payment_{bank_payment_id}.png")
 
         return {"bank_payment_id": bank_payment_id,
                 "qr_code_path": f"qr_code_payment_{bank_payment_id}"}
